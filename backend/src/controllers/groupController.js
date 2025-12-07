464aa7631132eee93d7ee8178b0ccee621cf19c9
Vivek Sharma <vivek.codespace@gmail.com>
2025-12-07 15:26:31 +0530

    create-group and add-member endpoint done

--- backend/src/controllers/groupController.js
+++ backend/src/controllers/groupController.js
@@ -4,20 +4,38 @@ export async function createGroup(req, res) {
   const { name } = req.body;
   const userId = req.user.id;
 
-  const { data, error } = await supabase
+  const { data: group, error: groupError } = await supabase
     .from("groups")
     .insert([{ name, created_by: userId }])
     .select()
     .single();
 
-  if (error) return res.status(400).json({ error });
+  if (groupError) return res.status(400).json({ message:"Group not created",error:groupError});
 
-  res.json(data);
+  //add the member who created the group in that group first
+
+  const {error: memberError} = await supabase
+  .from("group_members")
+  .insert([{ group_id: group.id, user_id: userId}])
+  .select()
+  .single();
+
+  if(memberError) return res.status(400).json({
+    message:"Group created successfully but failed to add creator as member.",
+    error:memberError
+  })
+
+  return res.json({
+    success: true,
+    message:"Group creation and member addition successful",
+    group
+  })
+
 }
 
 export async function addMember(req, res) {
   const { group_id, user_id } = req.body;
-
+  
   const { data, error } = await supabase
     .from("group_members")
     .insert([{ group_id, user_id }])
--- backend/src/middleware/authMiddleware.js
+++ backend/src/middleware/authMiddleware.js
@@ -3,7 +3,7 @@ import jwt from "jsonwebtoken";
 export function protect(req, res, next) {
   try {
     const token = req.headers.authorization?.split(" ")[1];
-
+      
     if (!token) return res.status(401).json({ message: "No token provided" });
 
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
