61f40597720972f6151d75cc6337fa604de875cd
Vivek Sharma <vivek.codespace@gmail.com>
2025-12-14 16:47:43 +0530

    made frontend deploy ready

--- frontend/src/services/apiClient.jsx
+++ frontend/src/services/apiClient.jsx
@@ -1,19 +1,18 @@
 import axios from "axios";
 
 const apiClient = axios.create({
-  baseURL: "http://localhost:5000", // your backend
+  baseURL: import.meta.env.VITE_API_URL,
   headers: {
     "Content-Type": "application/json",
-  },
+  }
 });
 
-apiClient.interceptors.request.use((config)=>{
+apiClient.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
-  if(token){
+  if (token) {
     config.headers.Authorization = `Bearer ${token}`;
-
   }
   return config;
-})
+});
 
 export default apiClient;
